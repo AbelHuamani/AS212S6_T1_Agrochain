937f4dc536bda16983d0cd4deff313e3912f4104
Abel Huamani Espinoza <86270180+AbelHuamani@users.noreply.github.com>
2024-06-05 07:13:02 -0500

    siu

---
+++ src/app/interfaces/producto.ts
@@ -0,0 +1,16 @@
+export interface  Producto {
+    id?: number;
+    imagenurl: string;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    fechainicio: string;
+    fechaculminacion: string;
+    telefono: string;
+    ubicacion: string;
+    wallet: string;
+    estado: string;
+    isFlipped: boolean;
+    flip: boolean;
+  }
+  
